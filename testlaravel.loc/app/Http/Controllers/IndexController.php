8d3661bdc337cdb32aaeebec919484604c985aa8
ilonabryk <ilonkabryk127@gmail.com>
2018-03-29 20:44:57 +0300

    lab3_1

--- testlaravel.loc/app/Http/Controllers/IndexController.php
+++ testlaravel.loc/app/Http/Controllers/IndexController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 //use App\Http\Requests;
 
+use App\Article;
+
 class IndexController extends Controller
 {
     //
@@ -14,7 +16,13 @@ class IndexController extends Controller
     	$header = 'Hello World!!!';
     	$message = 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.';
 		
-		return view('page')->with(['message'=>$message,'header' => $header]);
+		$articles = Article::select(['id', 'title', 'descr'])->get();
+		
+		//dump($articles);
+		
+	return view('page')->with(['message'=>$message,
+		'header' => $header, 
+		'articles' => $articles]);
 		
 	}
     
--- testlaravel.loc/resources/views/page.blade.php
+++ testlaravel.loc/resources/views/page.blade.php
@@ -1,33 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
+@extends('layouts.site')
 
-    <title>Jumbotron Template for Bootstrap</title>
+@section('content')
 
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/jumbotron.css') }}" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <ul id="navbar" class="menu">
-                    <li><a href="https://laravel.com/docs">Documentation</a></li>
-                    <li><a href="https://laracasts.com">Laracasts</a></li>
-                    <li><a href="https://laravel-news.com">News</a></li>
-                    <li><a href="https://forge.laravel.com">Forge</a></li>
-                    <li><a href="https://github.com/laravel/laravel">GitHub</a></li>
-        </ul><!--/.navbar-collapse -->
-      </div>
-    </nav>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
+<!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
         <h1>Hello, world!</h1>
@@ -39,21 +14,17 @@
     <div class="container">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+      
+      @foreach($articles as $article)
+      
+      <div class="col-md-4">
+          <h2>{{ $article->title }}</h2>
+          <p> {{ $article-> descr }} </p>
+          <p><a class="btn btn-default" href="#" role="button">Details &raquo;</a></p>
         </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
+      
+      @endforeach
+        
       </div>
 
       <hr>
@@ -62,6 +33,5 @@
         <p>&copy; 2016 Company, Inc.</p>
       </footer>
     </div> <!-- /container -->
-
-  </body>
-</html>
+    
+    @endsection
